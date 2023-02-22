27ef2042689c36c1e71b5c7a7fab0f43b7115e69
FC333 <2990299255@qq.com>
2023-02-22 13:25:19 +0800

    1 commit test

--- Test/src/test/java/com/zkx/ATest.java
+++ Test/src/test/java/com/zkx/ATest.java
@@ -18,6 +18,7 @@ public class ATest {
         System.out.println(3);
         System.out.println(4);
         System.out.println(5);
-System.out.println(6);
+        System.out.println(6);
+        System.out.println(7);
     }
 }
