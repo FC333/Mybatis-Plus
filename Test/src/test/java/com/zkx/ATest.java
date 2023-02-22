dbc765a5c73efb648dc45fed0837ed3e3fc3f8b9
FC333 <51044365+FC333@users.noreply.github.com>
2023-02-22 13:24:06 +0800

    Update ATest.java

--- Test/src/test/java/com/zkx/ATest.java
+++ Test/src/test/java/com/zkx/ATest.java
@@ -17,5 +17,7 @@ public class ATest {
         System.out.println(2);
         System.out.println(3);
         System.out.println(4);
+        System.out.println(5);
+System.out.println(6);
     }
 }
