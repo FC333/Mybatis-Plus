89261040664284fc87f07028938c49fa3401cd47
FC333 <2990299255@qq.com>
2023-02-22 11:09:44 +0800

    10 commit test

--- Test/src/test/java/com/zkx/ATest.java
+++ Test/src/test/java/com/zkx/ATest.java
@@ -15,9 +15,6 @@ public class ATest {
         System.out.println(1);
         System.out.println("master1");
         System.out.println("b1");
-        System.out.println(12);
-        System.out.println(13);
-        System.out.println(14);
-        System.out.println(15);
+
     }
 }
